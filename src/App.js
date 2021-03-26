883be11298dfefaaff9689947d6b54a710d8e113
IYassineBayoudhI <yassine.bayoudh@outlook.com>
2021-03-26 17:49:16 +0100

    Adding userContext

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -19,22 +19,30 @@ import Events from "./components/BackOffice/Events";
 import AddPostForm from "./components/Posts/addPostForm";
 import UpdatePostForm from "./components/Posts/updatePostForm";
 import PostDetails from "./components/Posts/postDetails";
+import { UserContext } from "./contexts/userContext";
 
 function App() {
+  const [connectedUser, setConnectedUser] = useState({
+    _id: "605cb3aefb9ba3244878f644",
+    username: "Rana",
+    password: "yayaya",
+  });
   return (
     <>
-      <Navbar />
-      <Switch>
-        <Route path='/about' component={About} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/user/profile' component={Profile}></Route>
-        <Route path='/event/addPost' component={AddPostForm} />
-        <Route path='/event/post' component={PostDetails} />
-        <Route path='/event/updatePost/:id' component={UpdatePostForm} />
+      <UserContext.Provider value={[connectedUser, setConnectedUser]}>
+        <Navbar />
+        <Switch>
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/user/profile' component={Profile}></Route>
+          <Route path='/event/addPost' component={AddPostForm} />
+          <Route path='/event/post' component={PostDetails} />
+          <Route path='/event/updatePost/:id' component={UpdatePostForm} />
 
-        <Route exact to='/' component={Home} />
-      </Switch>
-      <Footer />
+          <Route exact to='/' component={Home} />
+        </Switch>
+        <Footer />
+      </UserContext.Provider>
 
       {/*<NavbarBack></NavbarBack>
       <SideBar></SideBar>
