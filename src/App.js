e9fc508de1bb54b2a06f85077e32ba2eb6f1ac96
IYassineBayoudhI <yassine.bayoudh@outlook.com>
2021-03-28 19:09:50 +0100

    fix claim display

--- src/App.js
+++ src/App.js
@@ -26,11 +26,10 @@ function App() {
     _id: "6041f2fe9dbc16c1758d7a9a",
     username: "Rana",
     password: "yayaya",
-    role: "admin",
+    role: "aa",
   });
   return (
     <>
-
       {(() => {
         if (connectedUser.role === "admin") {
           return (
@@ -38,10 +37,10 @@ function App() {
               <NavbarBack></NavbarBack>
               <SideBar></SideBar>
               <Switch>
-                <Route path="/ClaimsBack" component={ClaimBack} />
-                <Route path="/Products" component={Products} />
-                <Route path="/Events" component={Events} />
-                <Route exact to="/" component={DashBoard} />
+                <Route path='/ClaimsBack' component={ClaimBack} />
+                <Route path='/Products' component={Products} />
+                <Route path='/Events' component={Events} />
+                <Route exact to='/' component={DashBoard} />
               </Switch>
               <FooterBack></FooterBack>
             </>
@@ -52,18 +51,17 @@ function App() {
               <UserContext.Provider value={[connectedUser, setConnectedUser]}>
                 <Navbar></Navbar>
                 <Switch>
-                  <Route path="/about" component={About} />
-                  <Route path="/contact" component={Contact} />
-                  <Route path="/user/profile" component={Profile} />
-                  <Route path="/event/addPost" component={AddPostForm} />
-                  <Route path="/event/post/:id" component={PostDetails} />
+                  <Route path='/about' component={About} />
+                  <Route path='/contact' component={Contact} />
+                  <Route path='/user/profile' component={Profile} />
+                  <Route path='/event/addPost' component={AddPostForm} />
+                  <Route path='/event/post/:id' component={PostDetails} />
                   <Route
-                    path="/event/updatePost/:id"
+                    path='/event/updatePost/:id'
                     component={UpdatePostForm}
                   />
 
-
-                  <Route exact to="/" component={Home} />
+                  <Route exact to='/' component={Home} />
                 </Switch>
                 <Footer />
               </UserContext.Provider>
