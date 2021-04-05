b56650c1e1cc26bd765aa3a15f9ae4720d1f2fee
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2021-04-05 14:45:54 +0200

    rendering the events components

--- src/components/Events/homeEvent.js
+++ src/components/Events/homeEvent.js
@@ -1,103 +1,125 @@
 import React, { Component } from 'react';
 import {Card , CardImg, CardImgOverlay, CardTitle , Breadcrumb , BreadcrumbItem} from 'reactstrap';
 import {Link} from 'react-router-dom';
+import { fetchEvents } from '../../redux/slices/eventSlice';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 
 
-function RenderEventItem ({events , onClick }){
-    return(
-        <Card>
-            
-                <CardImg />
-                <CardImgOverlay>
-                    <CardTitle></CardTitle>
-                </CardImgOverlay>
-            
-        </Card>
-    );
-}
+
+const mapStateToProps = state => ({
+    events: state.eventSlice.events
+});
+
+const mapDispatchToProps = dispatch => ({
+
+    fetchEvents: () => dispatch(fetchEvents())
+});
 
 
-      /*  const listEvents = props.dishes.dishes.map((dish) => {
-            return(
-                <div key={} className="col-12 col-md-5 m-1">
-                   <RenderEventItem events={events} />
-                </div>
-            );
-        });*/
 
-        const HomeEvent = (props) => {
-            return(
-                <>
-                  <div class="breadcrumb-section">
-                      <div class="container">
-                      <div className="row">
-                              <Breadcrumb>
-                                  <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                                  <BreadcrumbItem active>Events</BreadcrumbItem>
-                              </Breadcrumb>
-                              <div className="col-12">
-                                  <h3>Events</h3>
-                                  <hr />
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-  
-                  <section class="section-b-space blog-page ratio2_3">
-                      <div class="container">
-                          <div class="row">
-                          
-                              <div class="col-xl-3 col-lg-4 col-md-5">
-                                  <div class="blog-sidebar">
-                                      <div class="theme-card">
-                                          <h4>Recent Events</h4>
-                                      </div>
-                                      <div class="theme-card">
-                                          <h4>Popular Events</h4>
-                                      </div>
+class HomeEvent extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+
+    componentDidMount(){
+        this.props.fetchEvents();
+    }
+    
+
+    render(){
+
+        const RenderEventDetails = ({ event }) => {
+            return (
+                
+                        <div class="row blog-media">
+                              <div class="col-xl-6">
+                                  <div class="blog-left">
+                                      <a href="#"><img src="../assets/images/blog/1.jpg"
+                                              class="img-fluid blur-up lazyload bg-img" alt=""/>
+                                          </a>
                                   </div>
                               </div>
+                              <div class="col-xl-6">
+                      <div class="blog-right">
+                          <div>
+                              <h6> opening date : {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(event.date_debut)))}</h6><a href="#">
+                                  <h4>{event.eventName}</h4>
+                              </a>
+                              <ul class="post-social">
+                                  <li>Posted By : {event.user_id}</li>
+                                  <li><i class="fa fa-comments"></i> 10 Posts</li>
+                              </ul>
+                              <p>{event.description}</p>
+                          </div>
+                        </div>
+                        </div>
+                        </div>
+            );
+        }
 
-                              <div class="col-xl-9 col-lg-8 col-md-7 order-sec">
-                              <div class="row blog-media">
-                                    <div class="col-xl-6">
-                                        <div class="blog-left">
-                                            <a href="#"><img src="../assets/images/blog/1.jpg"
-                                                    class="img-fluid blur-up lazyload bg-img" alt=""/>
-                                                </a>
-                                        </div>
-                                    </div>
-                                    <div class="col-xl-6">
-                            <div class="blog-right">
-                                <div>
-                                    <h6>25 January 2018</h6><a href="#">
-                                        <h4>you how all this mistaken idea of denouncing pleasure and praising pain was
-                                            born.</h4>
-                                    </a>
-                                    <ul class="post-social">
-                                        <li>Posted By : Admin Admin</li>
-                                        <li><i class="fa fa-heart"></i> 5 Hits</li>
-                                        <li><i class="fa fa-comments"></i> 10 Comment</li>
-                                    </ul>
-                                    <p>Consequences that are extremely painful. Nor again is there anyone who loves or
-                                        pursues or desires to obtain pain of itself, because it is pain, but because
-                                        occasionally circumstances occur in which toil and pain can procure him some
-                                        great pleasure.</p>
+        const RenderEvent = (props) =>{
+            const list_events = this.props.events.map((event) => {
+                console.log(event);
+                return(
+                <div key={event.id}>
+                    <RenderEventDetails event={event} />
+                    <br/>
+                </div>
+                );
+            });
+    
+            return(
+                <section class="section-b-space blog-page ratio2_3">
+                <div class="container">
+                    <div class="row">
+                    
+                        <div class="col-xl-3 col-lg-4 col-md-5">
+                            <div class="blog-sidebar">
+                                <div class="theme-card">
+                                    <h4>Recent Events</h4>
+                                </div>
+                                <div class="theme-card">
+                                    <h4>Popular Events</h4>
                                 </div>
                             </div>
                         </div>
-                              </div>
-                            </div>
+                        <div class="col-xl-9 col-lg-8 col-md-7 order-sec">
+                        {list_events}
+                        </div>
+                    </div>
+                </div>
+                </section>
+            );
+        }
+
+        return(
+            <>
+              <div class="breadcrumb-section">
+                  <div class="container">
+                  <div className="row">
+                          <Breadcrumb>
+                              <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                              <BreadcrumbItem active>Events</BreadcrumbItem>
+                          </Breadcrumb>
+                          <div className="col-12">
+                              <h3>Events</h3>
+                              <hr />
                           </div>
                       </div>
-                  </section>
-  
-                </>
-            );
+                  </div>
+              </div>
+                <RenderEvent />
+            </>
+        );
 
-        }
-          
+    }
 
 
-export default HomeEvent;
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(HomeEvent));
--- src/redux/slices/eventSlice.js
+++ src/redux/slices/eventSlice.js
@@ -9,11 +9,15 @@ let initialState = {
 };
 
 const eventSlice = createSlice({
-    name: "event",
+    name: "events",
     initialState,
     reducers:{
+        listEvents(state ,action){
+            state.events = action.payload;
+        },
         addEvent: (state , action) => {
-            // action
+            const payload = action.payload;
+            state.events.push(payload);
         },
         updateEvent: (state , action) => {
             //
@@ -21,13 +25,24 @@ const eventSlice = createSlice({
         deleteEvent: (state , action) => {
             //
         },
-        setErrors: (state,action) => {
-
+        setErrors(state,action){
+            state.errors = action.payload;
         }
     }
 });
 
+export const fetchEvents = () => async (dispatch)  => {
+    const [res, error] = await queryApi("event/getAllEvents");
 
-export const { addEvent , updateEvent , deleteEvent , setErrors } = eventSlice.actions;
+    if(error){
+        dispatch(setErrors(error));
+    } else{
+        dispatch(listEvents(res));
+        console.log("im here");
+    }
+}
+
+
+export const { listEvents, addEvent , updateEvent , deleteEvent , setErrors } = eventSlice.actions;
 
 export default eventSlice.reducer;
