7e172d969758ad4b1e289ac1cf0ed623ca1f4808
daghbari med ali <48735426+dagh1@users.noreply.github.com>
2021-04-16 18:31:23 +0200

    add size

--- src/components/3D/bodyDetection.js
+++ src/components/3D/bodyDetection.js
@@ -1,32 +1,91 @@
 import React, { useRef, useState } from "react";
-import * as tf from "@tensorflow/tfjs";
+
 import * as bodyPix from "@tensorflow-models/body-pix";
 import Webcam from "react-webcam";
-import { useParams } from "react-router-dom";
+
 import Stats from 'stats.js';
 import jwtDecode from "jwt-decode";
 
-  function size (props)  {
+  function CalculateSize (props)  {
   
-   let user;
+    let user; 
+   const [size,setsize]=useState();
    const jwtToken = localStorage.getItem("jwt");
    if (jwtToken) {
      // Set auth token header auth
      user = jwtDecode(jwtToken); // Decode token and get user info and exp
      
    }
-   
+   if(user.height) {
+   const top= props.person.keypoints[1].position.y 
+   const tall= props.person.keypoints[17].position.y- top; 
+   const ratio=user.height/tall;
+   const widthx = Math.abs(
+    props.person.keypoints[5]["position"]["x"] - props.person.keypoints[6]["position"]["x"]
+  );
+if (widthx*ratio > 0.889 && widthx*ratio <0.9398) {
+setsize("S");
+}
+else if
+(widthx*ratio > 0.9398 && widthx*ratio < 1.016 ){
+  setsize("M");
+}
+   else if
+(widthx*ratio > 1.016 && widthx*ratio < 1.0922 ){
+  setsize("L");
+}
+else if
+(widthx*ratio > 1.0922 && widthx*ratio <1.1684 ){
+  setsize("XL");
+}
+else if
+(widthx*ratio > 1.1684 && widthx*ratio < 1.2446 ){
+  setsize("XXL");
+}
+   }
    
    return (
      <>
-     <h3></h3>
+     <h3>{size}</h3>
      </>);
 }
+function PutClothes (props){
 
+  const imageRef = useRef(null);
+const personDetail = props.person;
+  const x = personDetail?.allPoses[0]?.keypoints[6]["position"]["x"];
+  const y = personDetail?.allPoses[0]?.keypoints[6]["position"]["y"];
+  // console.log(person);
+  const widthx = Math.abs(
+    personDetail?.allPoses[0]?.keypoints[5]["position"]["x"] - personDetail?.allPoses[0]?.keypoints[6]["position"]["x"]
+  );
+
+  const image = imageRef.current;
+  image.style.top = y + 120 + "px";
+  image.style.left = x + "px";
+  image.style.width = widthx + "px";
+
+
+
+  return (
+    <>
+    <img
+        ref={imageRef}
+   alt=""
+        style={{
+          position: "absolute",
+          zindex: 10,
+          width: 50,
+          height: 40,
+        }}
+      />
+    </>);
+}
  
 
+
 function BodyDetection() {
-  const { imgurl } = useParams();
+
   const isAndroid = () => {
     return /Android/i.test(navigator.userAgent);
   };
@@ -50,8 +109,8 @@ function BodyDetection() {
   // const canvas = document.querySelector("canvas");
   const webcamRef = useRef(null);
   const [net, setNet] = useState();
-  const imageRef = useRef(null);
- const [bodysize, setbodysize] = useState();
+ 
+ const [person, setperson] = useState();
   // const ctx = canvas?.getContext("2d");
   const getnet = async () => {
     const lnet =
@@ -85,7 +144,7 @@ function BodyDetection() {
    
     
   
-      const person = await net.segmentPersonParts(webcamRef.current.video, {
+      const personDetail = await net.segmentPersonParts(webcamRef.current.video, {
         flipHorizontal: false,
         internalResolution: "medium",
         segmentationThreshold: 0.7,
@@ -95,21 +154,9 @@ function BodyDetection() {
         refineSteps: 10,
       });
 
-    if (person) {
-      console.log(person);
-        const x = person?.allPoses[0]?.keypoints[6]["position"]["x"];
-        const y = person?.allPoses[0]?.keypoints[6]["position"]["y"];
-        // console.log(person);
-        const widthx = Math.abs(
-          person?.allPoses[0]?.keypoints[5]["position"]["x"] - x
-        );
-
-        const image = imageRef.current;
-        image.style.top = y + 120 + "px";
-        image.style.left = x + "px";
-        image.style.width = widthx + "px";
-
-        // console.log(image.style.top);
+    if (personDetail) {
+      setperson(personDetail);
+       
       }
     
   };
@@ -136,17 +183,10 @@ function BodyDetection() {
           videoConstraints:{videoConstraints}
         }}
       />
-      <img
-        ref={imageRef}
-        src={imgurl}
-        style={{
-          position: "absolute",
-          zindex: 10,
-          width: 50,
-          height: 40,
-        }}
-      />
-      {stats.showPanel(0)}
+     
+    
+      <PutClothes person={person.allPoses[0]} />
+      <CalculateSize person={person.allPoses[0]} />
     </>
   );
 }
