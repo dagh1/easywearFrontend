01e718f2772df5f6b12bf3fe212c026ab6a5473a
AlouiOmar97 <60584294+AlouiOmar97@users.noreply.github.com>
2021-05-02 07:12:13 +0100

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,6 +38,7 @@ const Navbar = () => {
                   </li>
                   <li className="onhover-dropdown mobile-account">
                     <i className="fa fa-user" aria-hidden="true" />
+                    {/* user */}
                     My Account
                     <ul className="onhover-show-div">
                       {!user && (
